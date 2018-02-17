ccd6df0cc9961d688d7da8feb2d335585f544288
Rully Ramanda <krooco@gmail.com>
2018-02-17 11:45:29 +0700

    Rename accessToken to token

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ function createInstance() {
 
   instance.interceptors.request.use((config) => {
     const cfg = Object.assign({}, config);
-    const token = window.localStorage.getItem('accessToken');
+    const token = window.localStorage.getItem('token');
     cfg.headers.Authorization = token || '';
     return cfg;
   });
