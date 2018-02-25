5aca6ff17e8e2b88774815be1c31ba2b2a60334d
Rully Ramanda <krooco@gmail.com>
2018-02-25 23:22:29 +0700

    Remove pagination from students table

--- src/views/Students/StudentsTable.js
+++ src/views/Students/StudentsTable.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import {
   Table,
-  Pagination,
-  PaginationItem,
-  PaginationLink,
   Badge,
   Button
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
 
 export default class StudentsTable extends Component {
   renderRow(item) {
@@ -50,26 +48,6 @@ export default class StudentsTable extends Component {
             }
           </tbody>          
         </Table>
-        <Pagination>
-          <PaginationItem>
-            <PaginationLink previous href="#"></PaginationLink>
-          </PaginationItem>
-          <PaginationItem active>
-            <PaginationLink href="#">1</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">2</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">3</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href="#">4</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink next href="#"></PaginationLink>
-          </PaginationItem>
-        </Pagination>
       </div>
     );
   }
